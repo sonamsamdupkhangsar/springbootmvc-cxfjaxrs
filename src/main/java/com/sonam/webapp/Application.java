a9411263971f7ab736bca983a49e2f3b643dc48c
Sonam Samdupkhangsar <Sonam.Samdupkhangsar@imail.org>
2017-03-28 13:37:42 -0600

    rename to webserviceconfig for jaxrs endpoint

--- src/main/java/com/sonam/webapp/Application.java
+++ src/main/java/com/sonam/webapp/Application.java
@@ -1,37 +1,11 @@
 package com.sonam.webapp;
 
-import com.sonam.webapp.jaxrs.cxf.WelcomeGreeting;
-import org.apache.cxf.Bus;
-import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
-
-
-    @Autowired
-    private Bus bus;
-
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Server rsServer() {
-        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
-        endpoint.setBus(bus);
-        endpoint.setAddress("/a");
-        // Register 2 JAX-RS root resources supporting "/sayHello/{id}" and "/sayHello2/{id}" relative paths
-        endpoint.setServiceBeans(Arrays.<Object>asList(new WelcomeGreeting()));
-        endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
-        return endpoint.create();
     }
 }
--- src/main/java/com/sonam/webapp/config/WebserviceConfig.java
+++ src/main/java/com/sonam/webapp/config/WebserviceConfig.java
@@ -2,9 +2,11 @@ package com.sonam.webapp.config;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.sonam.webapp.jaxrs.Greeting;
+import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,12 +16,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses = Greeting.class)
-public class AppConfig {
+public class WebserviceConfig {
     @Autowired
     private Greeting greeting;
+
+    @Autowired
+    private Bus bus;
 
     @Bean(destroyMethod = "shutdown")
     public SpringBus cxf() {
@@ -44,5 +51,16 @@ public class AppConfig {
                 new ServletRegistrationBean(new CXFServlet(), "/api/*");
         servletRegistrationBean.setLoadOnStartup(1);
         return servletRegistrationBean;
+    }
+
+    @Bean
+    public Server rsServer() {
+        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+        endpoint.setBus(bus);
+        endpoint.setAddress("/greeting");
+        // Register 2 JAX-RS root resources supporting "/sayHello/{id}" and "/sayHello2/{id}" relative paths
+        endpoint.setServiceBeans(Arrays.<Object>asList(greeting));
+        endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
+        return endpoint.create();
     }
 }
